fdee94921fd2b9910987f70cd9e9020243e6c272
HarryRadit <harrydxm@gmail.com>
2024-06-09 19:22:47 +0700

    post time donesdao

--- pygame/pong_game.py
+++ pygame/pong_game.py
@@ -30,8 +30,8 @@ ball_x = window_width // 2
 ball_y = window_height // 2
 
 ball_radius = 10
-ball_x = 3
-ball_y = -3
+ball_speedx = 3
+ball_speedy= -3
 
 #score
 score = 0
@@ -47,13 +47,60 @@ while not game_over:
             pygame.quit()
             sys.exit()
 
+        #move the paddle
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d] and paddle_x > 0:
+            paddle_x += 5
+        if keys[pygame.K_a] and paddle_x < window_width - paddle_width:
+            paddle_x -= 5
+
+        #move the ball
+        ball_x += ball_speedx
+        ball_y += ball_speedy
+
+
 
     #draw everything
 
     window.fill(white)
     paddle = pygame.draw.rect(window, black, (paddle_x, paddle_y, paddle_height, paddle_width))
-    ball = pygame.draw.circle(window, black, (ball_x + 200, ball_y +240), ball_radius)
+    ball = pygame.draw.circle(window, black, (ball_x , ball_y ), ball_radius)
+
+    score_text = font.render("Score: " + str(score), True, black)
+    window.blit(score_text, (10, 10))
 
     pygame.display.update()
-    print(())
+
+
+    # ball collsion with walls
+    if ball.left <= 0 or ball.right >= window_width:
+        ball_speedx *= -1
+    if ball.top <= 0 or ball.bottom >= window_height:
+        ball_speedy *= -1
+
+    #ball collsion with paddle
+    if pygame.Rect.colliderect(ball,paddle):
+        ball_speedy *= -1
+        score += 100000
+
+    if ball.bottom >= window_height:
+        game_over = True
+window.fill(red)
+score_text = font.render("Score: " + str(score), True, black)
+window.blit(score_text, (10, 10))
+game_over_text = font.render("Game Over", True, black)
+window.blit(game_over_text, (window_width // 2 - 100, window_height // 2))
+pygame.display.update()
+
+pygame.time.delay(2000)
+pygame.quit()
+sys.exit()
+
+
+
+pygame.display.update()
+pygame.time.delay(30)
+
+pygame.quit()
+sys.exit()
 
