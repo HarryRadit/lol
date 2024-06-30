9dd8a49bda70c4b5f68a739f84d1aadc91d4b338
HarryRadit <harrydxm@gmail.com>
2024-06-30 19:21:06 +0700

    CITYY!!!

--- City_collider/City_collider.py
+++ City_collider/City_collider.py
@@ -17,7 +17,7 @@ background1 = pygame.transform.scale(background1, (window_width, window_height))
 background2 = pygame.transform.scale(background1, (window_width, window_height))
 
 #load car image
-car_img = pygame.image.load('download (15).jpeg').convert_alpha()
+car_img = pygame.image.load('images__6_-removebg-preview.png').convert_alpha()
 car_width, car_height = car_img.get_size()
 car_scale = 0.15
 car_img = pygame.transform.scale(car_img, (int(car_width*car_scale), int(car_height*car_scale)))
@@ -27,7 +27,7 @@ car_x = window_width // 2 - car_width // 2
 car_y = window_height - 50
 car_speed = 3
 
-class Asteroid:
+class Anothercar:
     def __init__(self, x, y, image, scale):
         self.x = x
         self.y = y
@@ -43,13 +43,13 @@ class Asteroid:
     def draw(self,window):
         window.blit(self.image, (self.x, self.y))
 
-anothercar_img = pygame.image.load("download (6).png").convert_alpha()
+anothercar_img = pygame.image.load("download__15_-removebg-preview.png").convert_alpha()
 anothercar_width, anothercar_height = anothercar_img.get_size()
 
 max_anothercar_scale = 0.15
 min_anothercar_scale = 0.05
 
-anothercar = []
+anothercars = []
 clock = pygame.time.Clock()
 game_running = True
 
@@ -62,3 +62,60 @@ def close_game():
 def update_background():
     global background1_y, background2_y
     background1_y = (background1_y + 1) % window_height
+    background2_y = background1_y - window_height
+    window.blit(background1, (0, background1_y))
+    window.blit(background2, (0, background2_y))
+
+background1_y = 0
+background2_y = window_height
+
+while game_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_running = False
+            close_game()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        car_x -= car_speed
+        if car_x < 0:
+            car_x = 0
+    if keys[pygame.K_RIGHT]:
+        car_x += car_speed
+        if car_x > window_width:
+            car_x = window_width - car_width
+
+    window.fill((0, 0, 0))
+    update_background()
+    window.blit(car_img, (car_x, car_y))
+
+
+    #another car spawn
+    if random.randint(0,100) < 2:
+        anothercar_x = random.randint(60, window_width - 120)
+        anothercar_scale = random.uniform(min_anothercar_scale, max_anothercar_scale)
+        anothercar = Anothercar(anothercar_x, -int(anothercar_height), anothercar_img, 0.2)
+        anothercars.append(anothercar)
+
+    #rectangles to represent the collision of cars
+    anothercar_rect = pygame.Rect(car_x, car_y, car_width, car_height)
+    
+    #move and draw another cars
+    for anothercar in anothercars:
+        anothercar.move(0.5)
+        anothercar_rect = pygame.Rect(anothercar.x, anothercar.y, anothercar_width, anothercar_height)
+        anothercar.draw(window)
+
+        if anothercar_rect.colliderect(anothercar_rect):
+            #game_running = False
+            #close_game()
+            pass
+    #remove the another cars  that have passed the screen
+    anothercars = [anothercar for anothercar in anothercars if anothercar.y < window_height]
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
+
