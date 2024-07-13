82d2bd0f948165d54d34a802fa054af23b3450b0
HarryRadit <harrydxm@gmail.com>
2024-07-13 19:26:18 +0700

    SSSSSSSSSSSSSSSSSSSS

---
+++ COUNTER!/CCOUNTER.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+
+window.title("COUNTER App")
+window.config(bg="lightblue")
+
+count = 0
+def increase():
+    global count
+    count += 1
+    label.config(text = "Counter: " + str(count))
+
+label = tk.Label(text="COUNTER", bg="red", fg="white", font=("Arial", 20))
+label.pack(pady = 10)
+
+increase_button = tk.Button(text="increase", bg="green", fg="white", font=("Arial", 20), command = increase)
+increase_button.pack(padx = 10, pady = 20)
+
+decrease_button = tk.Button(text="decrease", bg="blue", fg="white", font=("Arial", 20))
+decrease_button.pack(padx = 10, pady = 20)
+
+window.mainloop()
