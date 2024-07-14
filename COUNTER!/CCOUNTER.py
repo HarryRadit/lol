10e767c2925cfed833426b83cf4aa02cd8c42e7e
HarryRadit <harrydxm@gmail.com>
2024-07-14 19:21:54 +0700

    COUNTER

--- COUNTER!/CCOUNTER.py
+++ COUNTER!/CCOUNTER.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from playsound import playsound
 
 window = tk.Tk()
 
@@ -7,9 +8,18 @@ window.title("COUNTER App")
 window.config(bg="lightblue")
 
 count = 0
+
+
+
 def increase():
     global count
     count += 1
+    label.config(text = "Counter: " + str(count))
+    playsound('blazblue-continuum-shift-ii-counter.mp3')
+
+def decrease():
+    global count
+    count -= 1
     label.config(text = "Counter: " + str(count))
 
 label = tk.Label(text="COUNTER", bg="red", fg="white", font=("Arial", 20))
@@ -18,7 +28,7 @@ label.pack(pady = 10)
 increase_button = tk.Button(text="increase", bg="green", fg="white", font=("Arial", 20), command = increase)
 increase_button.pack(padx = 10, pady = 20)
 
-decrease_button = tk.Button(text="decrease", bg="blue", fg="white", font=("Arial", 20))
+decrease_button = tk.Button(text="decrease", bg="blue", fg="white", font=("Arial", 20), command = decrease)
 decrease_button.pack(padx = 10, pady = 20)
 
 window.mainloop()
